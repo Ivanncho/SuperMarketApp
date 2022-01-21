7df1a5834debb3a70cec8ba4f2a267aef94a421a
Ivanncho <jovanovski.ivanco@yahoo.com>
2022-01-21 18:33:21 +0100

    Adding Categories

---
+++ Plugin.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugin.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> _categories;
+        public CategoryInMemoryRepository()
+        {
+            _categories = new List<Category>() 
+            { 
+                new Category
+                {
+                    CategoryId = 1,
+                    Name = "Beverage",
+                    Description = "Beverage"
+                },
+                new Category
+                {
+                    CategoryId = 2,
+                    Name = "Food",
+                    Description = "Food"
+                },
+                new Category
+                {
+                    CategoryId = 3,
+                    Name = "Cosmetics",
+                    Description = "Cosmetics"
+                }
+            };
+        }
+        public IEnumerable<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UseCases/ViewCategoryUseCase.cs
@@ -0,0 +1,24 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace UseCases
+{
+    public class ViewCategoryUseCase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public ViewCategoryUseCase(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        public IEnumerable<Category> Execute()
+        {
+            return categoryRepository.GetCategories();
+        }
+    }
+}
