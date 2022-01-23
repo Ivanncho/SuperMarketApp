c5a84c9eb451a78ebe11e5ece39f3e9cd6c92d2b
Ivanncho <jovanovski.ivanco@yahoo.com>
2022-01-23 09:34:59 +0100

    Starting to work on delete category

---
+++ UseCases/DeleteCategoryUseCase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace UseCases
+{
+    public class DeleteCategoryUseCase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public DeleteCategoryUseCase(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        public void Delete(int category)
+        {
+
+        }
+    }
+}
