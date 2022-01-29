17945b1ba2809785ca0c74c8f69f3b3c16ea1fb9
Ivanncho <jovanovski.ivanco@yahoo.com>
2022-01-29 11:08:29 +0100

    working on repositories

---
+++ Plugin.DataStore.SQL/CategoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugin.DataStore.SQL
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly MarketContext context;
+
+        public CategoryRepository(MarketContext context)
+        {
+            this.context = context;
+        }
+        public void AddCategory(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            var categoryToRemove = context.Categories.Find(categoryId);
+
+            if(categoryToRemove == null)
+            {
+                return;
+            }
+
+            context.Categories.Remove(categoryToRemove);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return context.Categories.ToList();
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            return context.Categories.Find(categoryId);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            var categoryToUpdate = context.Categories.Find(category.CategoryId);
+            categoryToUpdate.Name = category.Name;
+            categoryToUpdate.Description = category.Description;
+            
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Plugin.DataStore.SQL/TransactionRepository.cs
@@ -0,0 +1,39 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugin.DataStore.SQL
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly MarketContext context;
+
+        public TransactionRepository(MarketContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<Transaction> Get(string cashierName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Transaction> GetByDateRange(string cashierName, DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Transaction> GetByDay(string cashierName, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string cashierName, int productId, string productName, double price, int beforeQty, int soldQty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
